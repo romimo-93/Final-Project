685941e453186a73bbc61691626080a3dd0a23c2
Melissa Monroe <everythinggoesnow@gmail.com>
2021-05-05 10:47:22 -0700

    add sample config

---
+++ src/config_sample.py
@@ -0,0 +1,9 @@
+# IF USING ATLAS (CLOUD) THE CONNECTION STRING IS AS FOLLOWS
+
+username="<username>"
+password="<password>"
+host="<host>"
+# The name of the database you are connecting to
+db_name="<databasename>"
+
+sql_conn="Driver={SQL Server};Server=;Database=" + db_name + ";uid=" + username + ";pwd="+ password
